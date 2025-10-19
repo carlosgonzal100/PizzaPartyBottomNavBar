280e8474324142f97e2fc52f7b35b733df35439e
Carlos <carlosgonzal100@gmail.com>
2025-10-18 20:50:14 -0400

    Finished TODO 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/PizzaPartyScreen.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/PizzaPartyScreen.kt
@@ -1,11 +1,8 @@
 package edu.farmingdale.pizzapartybottomnavbar
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -13,25 +10,19 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import kotlin.math.ceil
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 // ToDo 6: Add another level of hunger that is Hungry that is in between Medium and Very hungry FINISHED
 
 // ToDo 7: Using the ViewModel class, create a new ViewModel class called PizzaPartyViewModel as
 // a subclass of ViewModel. Add the following properties to the PizzaPartyViewModel - see Brightspace
 
+/** the old composable
 @Composable
 fun PizzaPartyScreen( modifier: Modifier = Modifier) {
     var totalPizzas by remember { mutableIntStateOf(0) }
@@ -150,4 +141,172 @@ fun calculateNumPizzas(
 
     return ceil(numPeople * slicesPerPerson / slicesPerPizza.toDouble()).toInt()
 }
+ */
+
+@Composable
+fun PizzaPartyScreen2(
+    modifier: Modifier = Modifier,
+    partyViewModel: PizzaPartyViewModel = viewModel()
+) {
+    Column(
+        modifier = modifier.padding(10.dp)
+    ) {
+        AppTitle(modifier)
+        PartySize(
+            numPeopleInput = partyViewModel.numPeopleInput,
+            onValueChange = { partyViewModel.numPeopleInput = it },
+            modifier = modifier
+        )
+        HungerLevelSelection(
+            hungerLevel = partyViewModel.hungerLevel,
+            onSelected = { partyViewModel.hungerLevel = it },
+            modifier = modifier
+        )
+        TotalPizzas(
+            totalPizzas = partyViewModel.totalPizzas,
+            modifier = modifier
+        )
+        CalculateButton(
+            onClick = { partyViewModel.calculateNumPizzas() },
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun AppTitle(modifier: Modifier = Modifier) {
+    Text(
+        text = "Pizza Party",
+        fontSize = 38.sp,
+        modifier = modifier.padding(bottom = 16.dp)
+    )
+}
+
+@Composable
+fun PartySize(
+    numPeopleInput: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    NumberField(
+        labelText = "Number of people?",
+        textInput = numPeopleInput,
+        onValueChange = { onValueChange(it) },
+        modifier = modifier.padding(bottom = 16.dp).fillMaxWidth()
+    )
+}
+
+@Composable
+fun HungerLevelSelection(
+    hungerLevel: HungerLevel,
+    onSelected: (HungerLevel) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    //modified this to have hungry
+    val hungerItems = listOf("Light", "Medium", "Hungry", "Very hungry")
+
+    RadioGroup(
+        labelText = "How hungry?",
+        radioOptions = hungerItems,
+        selectedOption = when (hungerLevel) {
+
+            //modified this to have hungry
+            HungerLevel.LIGHT -> hungerItems[0]
+            HungerLevel.MEDIUM -> hungerItems[1]
+            HungerLevel.HUNGRY -> hungerItems[2]
+            else -> hungerItems[3]
+        },
+        onSelected = {
+            onSelected(when (it) {
+                //modified this to have hungry
+                hungerItems[0] -> HungerLevel.LIGHT
+                hungerItems[1] -> HungerLevel.MEDIUM
+                hungerItems[2] -> HungerLevel.HUNGRY
+                else -> HungerLevel.VERYHUNGRY
+            })
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun TotalPizzas(
+    totalPizzas: Int,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "Total pizzas: $totalPizzas",
+        fontSize = 22.sp,
+        modifier = modifier.padding(top = 16.dp, bottom = 16.dp)
+    )
+}
+
+@Composable
+fun CalculateButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Text("Calculate")
+    }
+}
+
+@Composable
+fun NumberField(
+    labelText: String,
+    textInput: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = textInput,
+        onValueChange = onValueChange,
+        label = { Text(labelText) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun RadioGroup(
+    labelText: String,
+    radioOptions: List<String>,
+    selectedOption: String,
+    onSelected: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val isSelectedOption: (String) -> Boolean = { selectedOption == it }
+
+    Column {
+        Text(labelText)
+        radioOptions.forEach { option ->
+            Row(
+                modifier = modifier
+                    .selectable(
+                        selected = isSelectedOption(option),
+                        onClick = { onSelected(option) },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp)
+            ) {
+                RadioButton(
+                    selected = isSelectedOption(option),
+                    onClick = null,
+                    modifier = modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = option,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
 
